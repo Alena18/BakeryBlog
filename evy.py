1c24b48347e122f27b7ef4c049a9f311205afea2
Alena18 <>
2023-05-20 10:47:08 +0000

    Add settings

--- evy.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ["DATABASE_URL"]  = "postgres://mulrpunb:ohORYXWG8FDmTSKjDNWR50lE5in2fm8t@mel.db.elephantsql.com/mulrpunb"
-os.environ["SECRET_KEY"] = "7.sisterblog@ydacha"
-os.environ["CLOUDINARY_URL"] = "cloudinary://934995576437245:1l48UdynPagRbjZDqt2rjyxKyKc@dmie5ezad"
--- victoriabakery/urls.py
+++ victoriabakery/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('victoriabakeryblog.urls'), name='blog_urls'),
+    path('accounts/', include('allauth.urls')),
+    ]
