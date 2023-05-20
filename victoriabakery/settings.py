0baffe010f515f8f1e8a6962f37aadb925ea1410
Alena18 <>
2023-05-20 10:54:33 +0000

    Create models

--- victoriabakery/settings.py
+++ victoriabakery/settings.py
@@ -39,9 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
+    'django_summernote',
+    'readtime',
     'victoriabakeryblog',
 ]
 
